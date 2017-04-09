70f0c5e7f8c6526cec627ec831bbc536464ee787
Nick Arnold <nicka@labkey.com>
2017-04-09 10:36:50 -0700

    Assay

---
+++ src/labkey/dom/Assay.ts
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { buildURL } from '../ActionURL'
+import { request } from '../Ajax'
+import { getCallbackWrapper, getOnFailure, getOnSuccess } from '../Utils'
+
+interface FormWindow extends Window {
+    File: any
+    FormData: any
+}
+
+declare let window: FormWindow;
+
+interface IImportRunOptions {
+    assayId?: number
+    batchId?: number
+    batchProperties?: any
+    comment?: string
+    comments?: string
+    containerPath?: string
+    failure?: Function
+    files: Array<any>
+    name?: string
+    properties?: any
+    scope?: any
+    success: Function
+}
+
+export function importRun(options: IImportRunOptions): void {
+
+    if (!window.FormData) {
+        throw new Error('modern browser required');
+    }
+
+    let files = [];
+    if (options.files) {
+        for (let i = 0; i < options.files.length; i++) {
+            let f = options.files[i];
+            if (f instanceof window.File) {
+                files.push(f);
+            }
+            else if (f.tagName == 'INPUT') {
+                for (let j = 0; j < f.files.length; j++) {
+                    files.push(f.files[j]);
+                }
+            }
+        }
+    }
+
+    if (files.length == 0) {
+        throw new Error('At least one file is required');
+    }
+
+    let formData = new FormData();
+    formData.append('assayId', options.assayId);
+    formData.append('name', options.name);
+    formData.append('comment', options.comment);
+
+    if (options.batchId) {
+        formData.append('batchId', options.batchId);
+    }
+
+    if (options.properties) {
+        for (let key in options.properties) {
+            formData.append("properties['" + key + "']", options.properties[key]);
+        }
+    }
+
+    if (options.batchProperties) {
+        for (let key in options.batchProperties)
+            formData.append("batchProperties['" + key + "']", options.batchProperties[key]);
+    }
+
+    formData.append('file', files[0]);
+    for (let i = 1; i < files.length; i++) {
+        formData.append('file' + i, files[i]);
+    }
+
+    request({
+        url: buildURL('assay', 'importRun.api', options.containerPath),
+        method: 'POST',
+        success: getCallbackWrapper(getOnSuccess(options), options.scope),
+        failure: getCallbackWrapper(getOnFailure(options), options.scope, true /* isErrorCallback */),
+        form: formData
+    });
+}
