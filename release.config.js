95c3f378fee22aff966bb2e97951b026ae357b24
Nick Kerr <nicka@labkey.com>
2018-02-26 12:48:18 -0800

    Draft release build

---
+++ release.config.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+'use strict';
+
+var path = require("path");
+var webpack = require('webpack');
+
+var config = {
+
+    entry: {
+        'index': [
+            './src/labkey'
+        ]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ['babel-loader', 'ts-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    output: {
+        filename: 'lib/[name].js'
+    },
+
+    resolve: {
+        extensions: [ '.ts' ]
+    }
+};
+
+module.exports = config;
