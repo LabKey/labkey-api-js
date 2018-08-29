0e7ac0956d928403ba1c7f2144897900f66e159c
Nick Kerr <nicka@labkey.com>
2018-08-29 16:29:33 -0700

    Simplify removeQuotes

--- theme/helpers/removeQuotes.js
+++ theme/helpers/removeQuotes.js
@@ -1,12 +1,6 @@
 module.exports = {
     removeQuotes: function(str) {
-        var result = "";
-        for (var i = 0; i < str.length; i++) {
-            var char = str.charAt(i);
-            if(char != '\"') {
-                result += char;
-            }
-        }
-        return result;
+        if (str) return str.replace(/["]/g, '');
+        return str;
     }
-}
+};
