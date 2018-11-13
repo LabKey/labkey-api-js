035f6ed4dc3e2d750ef643d47eada9202434bda7
labkey-eyounske <eyounske@labkey.com>
2018-11-12 16:49:52 -0800

    Copyright and license updates   -- Update for November 2018   -- Now scanning .tf and .tfvars files (Terraform)   -- Deliberately leaving out SNPRC module (moving to Git very soon)

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 'use strict';
 
 import resolve from 'rollup-plugin-node-resolve';
