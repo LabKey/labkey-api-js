60d28815fe4710efd04e0c937a5df70918cf5a3b
labkey-nicka <nickk@labkey.com>
2021-03-04 15:23:58 -0800

    Remove List module

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,6 @@ import * as Exp from './labkey/Exp';
 import * as Experiment from './labkey/Experiment';
 import { FieldKey } from './labkey/FieldKey';
 import * as Filter from './labkey/Filter';
-import * as List from './labkey/List';
 import * as Message from './labkey/Message';
 import * as ParticipantGroup from './labkey/ParticipantGroup';
 import * as Pipeline from './labkey/Pipeline';
@@ -83,7 +82,6 @@ export {
     Experiment,
     FieldKey,
     Filter,
-    List,
     Message,
     ParticipantGroup,
     Pipeline,
