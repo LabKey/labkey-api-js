778791d87296c30ea2214fa98a766ff38d55de9f
ian <ians@labkey.com>
2021-04-16 16:47:21 -0700

    Add filter types update tree on filter type changes cleanup styling move filter icon to right
    TODO: use paths for SubTree filters

--- src/labkey/filter/Types.ts
+++ src/labkey/filter/Types.ts
@@ -140,18 +140,22 @@ export let Types: Record<string, IFilterType> = {
     //
     // Table/Query-wise operators
     //
-    Q: registerFilterType('Search', null, 'q', true, undefined, 'Search across all columns', undefined, undefined, true)
+    Q: registerFilterType('Search', null, 'q', true, undefined, 'Search across all columns', undefined, undefined, true),
+    //
+    // Ontology operators
+    //
+    ONTOLOGY_IN_SUBTREE : registerFilterType("Is In Subtree", null, "concept:insubtree", true,),
+    ONTOLOGY_NOT_IN_SUBTREE : registerFilterType("Is Not In Subtree", null, "concept:notinsubtree", true,)
 };
 
 export type JsonType = 'boolean' | 'date' | 'float' | 'int' | 'string';
 
-// TODO: Update to Record<JsonType, IFilterType[]>
 export const TYPES_BY_JSON_TYPE: Record<string, IFilterType[]> = {
     'boolean': [Types.HAS_ANY_VALUE, Types.EQUAL, Types.NEQ_OR_NULL, Types.ISBLANK, Types.NONBLANK],
     'date': [Types.HAS_ANY_VALUE, Types.DATE_EQUAL, Types.DATE_NOT_EQUAL, Types.ISBLANK, Types.NONBLANK, Types.DATE_GREATER_THAN, Types.DATE_LESS_THAN, Types.DATE_GREATER_THAN_OR_EQUAL, Types.DATE_LESS_THAN_OR_EQUAL],
     'float': [Types.HAS_ANY_VALUE, Types.EQUAL, Types.NEQ_OR_NULL, Types.ISBLANK, Types.NONBLANK, Types.GT, Types.LT, Types.GTE, Types.LTE, Types.IN, Types.NOT_IN, Types.BETWEEN, Types.NOT_BETWEEN],
     'int': [Types.HAS_ANY_VALUE, Types.EQUAL, Types.NEQ_OR_NULL, Types.ISBLANK, Types.NONBLANK, Types.GT, Types.LT, Types.GTE, Types.LTE, Types.IN, Types.NOT_IN, Types.BETWEEN, Types.NOT_BETWEEN],
-    'string': [Types.HAS_ANY_VALUE, Types.EQUAL, Types.NEQ_OR_NULL, Types.ISBLANK, Types.NONBLANK, Types.GT, Types.LT, Types.GTE, Types.LTE, Types.CONTAINS, Types.DOES_NOT_CONTAIN, Types.DOES_NOT_START_WITH, Types.STARTS_WITH, Types.IN, Types.NOT_IN, Types.CONTAINS_ONE_OF, Types.CONTAINS_NONE_OF, Types.BETWEEN, Types.NOT_BETWEEN]
+    'string': [Types.HAS_ANY_VALUE, Types.EQUAL, Types.NEQ_OR_NULL, Types.ISBLANK, Types.NONBLANK, Types.GT, Types.LT, Types.GTE, Types.LTE, Types.CONTAINS, Types.DOES_NOT_CONTAIN, Types.DOES_NOT_START_WITH, Types.STARTS_WITH, Types.IN, Types.NOT_IN, Types.CONTAINS_ONE_OF, Types.CONTAINS_NONE_OF, Types.BETWEEN, Types.NOT_BETWEEN],
 };
 
 // TODO: Update to Record<JsonType, IFilterType[]>
@@ -160,7 +164,8 @@ export const TYPES_BY_JSON_TYPE_DEFAULT: Record<string, IFilterType> = {
     'date': Types.DATE_EQUAL,
     'float': Types.EQUAL,
     'int': Types.EQUAL,
-    'string': Types.CONTAINS
+    'string': Types.CONTAINS,
+    'concept': Types.EQUAL,
 };
 
 /**
