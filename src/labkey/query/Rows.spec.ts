91033cc100bb13efb199ee3f96be891ddaac32cb
Nick Kerr <nicka@labkey.com>
2021-08-25 13:31:21 -0700

    43029: Support File/Attachment Fields (#110)

--- src/labkey/query/Rows.spec.ts
+++ src/labkey/query/Rows.spec.ts
@@ -15,6 +15,7 @@
  */
 import * as Ajax from '../Ajax'
 import {
+    bindFormData,
     deleteRows,
     insertRows,
     updateRows,
@@ -84,17 +85,33 @@ describe('insertRows', () => {
         form.append('test1', 'test2');
 
         // Act
-        (insertRows as any)({ schemaName, queryName, rows, form });
+        insertRows({ schemaName, queryName, rows, form });
 
         // Assert
         expect(requestSpy).toHaveBeenCalledWith(expect.objectContaining({
             method: 'POST',
             form: expect.anything(),
-            jsonData: expect.objectContaining({
-                queryName,
-                rows,
-                schemaName,
-            }),
+            url: '/query/insertRows.api',
+        }));
+        expect(requestSpy).toHaveBeenCalledWith(expect.not.objectContaining({
+            jsonData: expect.anything(),
+        }));
+    });
+
+    it('should support file data', () => {
+        // Arrange
+        const requestSpy = jest.spyOn(Ajax, 'request').mockImplementation();
+        const schemaName = 'SSS';
+        const queryName = 'QQQ';
+        const rows = [{ rowId: 1 }, { rowId: 2 }, { rowId: 3, someFile: new File([], '') }];
+
+        // Act
+        insertRows({ autoFormFileData: true, schemaName, queryName, rows });
+
+        // Assert
+        expect(requestSpy).toHaveBeenCalledWith(expect.objectContaining({
+            method: 'POST',
+            form: expect.anything(),
             url: '/query/insertRows.api',
         }));
     });
@@ -137,18 +154,47 @@ describe('updateRows', () => {
         form.append('test1', 'test2');
 
         // Act
-        (updateRows as any)({ schemaName, queryName, rows, form });
+        updateRows({ schemaName, queryName, rows, form });
 
         // Assert
         expect(requestSpy).toHaveBeenCalledWith(expect.objectContaining({
             method: 'POST',
             form: expect.anything(),
-            jsonData: expect.objectContaining({
-                queryName,
-                rows,
-                schemaName,
-            }),
             url: '/query/updateRows.api',
         }));
+        expect(requestSpy).toHaveBeenCalledWith(expect.not.objectContaining({
+            jsonData: expect.anything(),
+        }));
+    });
+});
+
+describe('bindFormData', () => {
+    const requestOptions = {
+        action: 'some.api',
+        autoFormFileData: true,
+        queryName: 'query',
+        schemaName: 'schema',
+    };
+    it('supports no form', () => {
+        expect(bindFormData({}, requestOptions)).toEqual(undefined);
+    });
+    it('supports user provided form', () => {
+        const expectedForm = new FormData();
+        expect(bindFormData({}, { ...requestOptions, form: expectedForm })).toEqual(expectedForm);
+    });
+    it('processes File data', () => {
+        // Arrange
+        const fileA = new File([], '');
+        const fileB = new File([], '');
+        const fileC = new File([], '');
+        const rows = [{ firstFile: fileA, rowId: 1 }, { rowId: 2, firstFile: fileC, secondFile: fileB }];
+
+        // Act
+        const form = bindFormData({ rows }, requestOptions, true);
+
+        expect(form).toBeDefined();
+        expect(form.get('firstFile::0')).toEqual(fileA);
+        expect(form.get('secondFile::1')).toEqual(fileB);
+        expect(form.get('firstFile::1')).toEqual(fileC);
     });
 });
