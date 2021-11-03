488adfaad94c82abd33b82ec93a7a49cbd097370
labkey-nicka <nickk@labkey.com>
2021-11-03 10:51:13 -0700

    Container: support effectivePermissions

--- src/labkey/constants.ts
+++ src/labkey/constants.ts
@@ -16,6 +16,8 @@
 export interface Container {
     /** Names of active modules in the container. */
     activeModules: string[];
+    /** Permissions the requesting user has in the container. */
+    effectivePermissions: string[];
     /** The name of the container's folder type. */
     folderType: string;
     /** Date format settings for this container. */
