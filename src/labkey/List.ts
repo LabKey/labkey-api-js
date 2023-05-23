928b5776060a33ce45fd00b806825e2078156108
Nick Kerr <nickk@labkey.com>
2023-05-23 09:24:53 -0700

    List: Update LABKEY.List.create utility (#154)

--- src/labkey/List.ts
+++ src/labkey/List.ts
@@ -15,65 +15,119 @@
  */
 import { create as createDomain, CreateDomainOptions, DomainDesign } from './Domain';
 
-export interface ICreateOptions {
-    domainDesign: DomainDesign;
-    /** The name of the key column.*/
-    keyName: string;
-    /** The type of the key column.  Either "int" or "string". */
+// The configuration options intermix the DomainDesign configuration with the CreateDomainOptions.
+// This can be very confusing, so we've limited the scope of which domain design configuration options are supported.
+// Users can specify all available DomainDesign configuration options by specifying `domainDesign` explicitly.
+export type DomainDesignOptions = Pick<DomainDesign, 'description' | 'fields' | 'indices' | 'name'>;
+
+export interface ListCreateOptions extends DomainDesignOptions, CreateDomainOptions {
+    /**
+     * @deprecated Use `options.keyName` instead.
+     * The name of the key column. The `options.keyName` takes precedence if both are specified.
+     */
+    keyName?: string;
+    /**
+     * @deprecated Use `kind` instead.
+     * The type of the key column. Can be `IntList`, `VarList`, or `AutoIncrementInteger`.
+     * The `kind` takes precedence if both are specified.
+     * Note: If the `AutoIncrementInteger` configuration is desired and you're specifying `kind` then the
+     * `kind` should be set to `IntList` and the `options.keyType` should be set to `AutoIncrementInteger`.
+     */
     keyType?: string;
-    kind?: string;
-    options?: any;
 }
 
 /**
- * Create a new list.
- * A primary key column must be specified with the properties 'keyName' and 'keyType'. If the key is not
- * provided in the domain design's array of fields, it will be automatically added to the domain.
+ * Create a new List.
+ * Lists support three types of primary key configurations; `IntList`, `VarList`, or `AutoIncrementInteger`.
+ * These key configurations determine what "kind" of List is created.
+ * If the key field configuration is not provided in the domain design's array of fields,
+ * it will be automatically added to the domain.
+ * Note: If a `domainDesign` is specified then it's configuration will take precedence over other `DomainDesign`
+ * properties that are specified.
  *
  * ```
+ *  // Creates a List with a string primary key.
  *  LABKEY.List.create({
- *      name: "mylist",
- *      keyType: "int",
- *      keyName: "one",
- *      description: "my first list",
  *      fields: [{
- *          name: "one", rangeURI: "int"
- *          },{
- *          name: "two", rangeURI: "multiLine", required: true
- *          },{
- *          name: "three", rangeURI: "Attachment"
- *      }]
+ *          name: 'partCode', rangeURI: 'string',
+ *      },{
+ *          name: 'manufacturer', rangeURI: 'string',
+ *      },{
+ *          name: 'purchaseDate', rangeURI: 'date',
+ *      }],
+ *      kind: 'VarList',
+ *      name: 'Parts',
+ *      options: {
+ *          keyName: 'partCode',
+ *          keyType: 'Varchar',
+ *      }
+ *  });
+ *
+ *  // Creates a List with an auto-incrementing primary key.
+ *  LABKEY.List.create({
+ *      description: 'teams in the league',
+ *      kind: 'IntList',
+ *      fields: [{
+ *          name: 'rowId', rangeURI: 'int',
+ *      },{
+ *          name: 'name', rangeURI: 'string', required: true,
+ *      },{
+ *          name: 'slogan', rangeURI: 'multiLine',
+ *      },{
+ *          name: 'logo', rangeURI: 'Attachment',
+ *      }],
+ *      name: 'Teams',
+ *      options: {
+ *          keyName: 'rowId',
+ *          keyType: 'AutoIncrementInteger',
+ *      }
  *  });
  * ```
  */
-export function create(config: ICreateOptions) {
+export function create(config: ListCreateOptions) {
+    // Separate out `ListCreateOptions` configuration
+    const { keyName, keyType, ...options } = config;
+
+    // Separate out `DomainDesignOptions` configuration
+    const { description, fields, indices, name, ...createDomainOptions } = options;
+
     const domainOptions: CreateDomainOptions = {
-        // not really awesome...intermixing interface with domain design. Separate these concerns.
-        domainDesign: config as Partial<DomainDesign>,
-        options: {},
+        ...createDomainOptions,
+        options: createDomainOptions.options ?? {},
     };
+
+    // If a `domainDesign` is not explicitly provided then fallback to `DomainDesignOptions` options
+    if (!domainOptions.domainDesign) {
+        domainOptions.domainDesign = { description, fields, indices, name };
+    }
 
     if (!domainOptions.domainDesign.name) {
         throw new Error('List name required');
     }
 
-    if (!config.kind) {
-        if (config.keyType == 'int') {
-            config.kind = 'IntList';
-        } else if (config.keyType == 'string') {
-            config.kind = 'VarList';
+    // Handle `keyType` only if `kind` is not specified
+    if (!domainOptions.kind) {
+        if (keyType === 'int' || keyType === 'IntList') {
+            domainOptions.kind = 'IntList';
+        } else if (keyType === 'string' || keyType === 'VarList') {
+            domainOptions.kind = 'VarList';
+            domainOptions.options.keyType = 'Varchar';
+        } else if (keyType === 'AutoIncrementInteger') {
+            domainOptions.kind = 'IntList';
+            domainOptions.options.keyType = 'AutoIncrementInteger';
+        } else {
+            throw new Error('List kind or keyType required');
         }
     }
 
-    if (config.kind != 'IntList' && config.kind != 'VarList') {
-        throw new Error('Domain kind or keyType required');
+    // Handle `keyName` only if `options.keyName` is not specified
+    if (!domainOptions.options.keyName) {
+        if (keyName) {
+            domainOptions.options.keyName = keyName;
+        } else {
+            throw new Error('List keyName or options.keyName required');
+        }
     }
-    domainOptions.kind = config.kind;
-
-    if (!config.keyName) {
-        throw new Error('List keyName required');
-    }
-    domainOptions.options.keyName = config.keyName;
 
     createDomain(domainOptions);
 }
