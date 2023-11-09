ad796709b9c63c6377c87201d8d4340d788b4993
labkey-nicka <nickk@labkey.com>
2023-11-09 08:51:25 -0800

    Add semicolon

--- src/labkey/Utils.ts
+++ src/labkey/Utils.ts
@@ -361,7 +361,7 @@ export function encodeHtml(html: string, retainEmptyValueTypes?: boolean): strin
         .replace(/'/g, '&#39;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
-        .replace(/\\/g, '&#92');
+        .replace(/\\/g, '&#92;');
 }
 
 export function escapeRe(s: string): string {
