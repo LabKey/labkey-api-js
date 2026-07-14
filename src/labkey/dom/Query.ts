dc1dfe1bb3708288fd6125573bf790c38dba659b
Xing Yang <5168106+XingY@users.noreply.github.com>
2026-07-14 12:56:18 -0700

    Multi genbank/fasta files import (#220)

--- src/labkey/dom/Query.ts
+++ src/labkey/dom/Query.ts
@@ -99,7 +99,7 @@ export interface IImportDataOptions {
     auditUserComment?: string;
     containerPath?: string;
     failure?: Function;
-    file?: File | Element | any;
+    file?: File | File[] | Element | any;
     format?: string;
     importIdentity?: any;
     importLookupByAlternateKey?: boolean;
@@ -167,7 +167,13 @@ export function importData(options: IImportDataOptions): XMLHttpRequest {
     }
 
     if (options.file) {
-        if (options.file instanceof File) {
+        if (Array.isArray(options.file)) {
+            options.file.forEach(f => {
+                if (f instanceof File) {
+                    form.append('file', f);
+                }
+            });
+        } else if (options.file instanceof File) {
             form.append('file', options.file);
         } else if (options.file.tagName == 'INPUT' && options.file.files.length > 0) {
             form.append('file', options.file.files[0]);
